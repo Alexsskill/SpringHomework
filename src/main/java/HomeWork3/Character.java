9b71a3039f57ff7e0a7a537dd8c13b144928679b
Alexsskill <harckovaleks@yandex.ru>
2025-06-03 15:28:25 +0400

    Homework1. arr1 + arr2 = arr3

---
+++ src/main/java/HomeWork3/Character.java
@@ -0,0 +1,5 @@
+package HomeWork3;
+
+public interface Character {
+    void attack();
+}
---
+++ src/main/java/HomeWork3/Mage.java
@@ -0,0 +1,8 @@
+package HomeWork3;
+
+public class Mage implements Character {
+    @Override
+    public void attack() {
+        System.out.println("Маг произносит заклинание!");
+    }
+}
